4d371f27566e071cb8113936237db53b5c455aad
DimaWide <dwchirocky@gmail.com>
2024-11-07 19:53:52 +0200

    add

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const PORT = process.env.PORT || 5000;
 
-console.log(12)
 
 // Используем стандартные middleware (например, для логирования)
 server.use(middlewares)
@@ -11,7 +11,8 @@ server.use(middlewares)
 // API с данными из db.json
 server.use(router)
 
+
 // Запускаем сервер на порту 3000
-server.listen(5000, () => {
+server.listen(PORT, () => {
   console.log('JSON Server is running on http://localhost:5000')
 })
